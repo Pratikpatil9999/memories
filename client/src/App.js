453d1274cfe82d06e669176c7b6f117370963e3a
Pratik <prateekpatil9999@gmail.com>
2021-09-14 11:20:30 +0530

    added style to posts from and post component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
+import memories from './images/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './styles';
 
 const App=()=>{
+    const classes=useStyles();
     return(
-        <h1>App</h1>
+        <Container maxWidth='lg'>
+            <AppBar classes={classes.appBar} position="static"  color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories
+                <img className={classes.image}src={memories} alt="memories" height="60"/></Typography>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={5}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
