83306a9570b4105c8a8505a88c7bc19fccd386bf
Pratik <prateekpatil9999@gmail.com>
2021-09-24 09:54:40 +0530

    making better to deploy

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    app.listen(PORT, () => console.log('Server is listening on port ' + PORT));
+    app.listen(PORT, () => console.log('Server is running on port ' + PORT));
   })
   .catch((error) => {
     console.log('Error: ' + error.message);
